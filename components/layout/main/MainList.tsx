1b10e640909bb09bf124c1d651db60af40feeb4b
GY <intherain522@naver.com>
2022-10-26 22:45:52 +0900

    feat : MainList 퍼블리싱

---
+++ components/layout/main/MainList.tsx
@@ -0,0 +1,32 @@
+import MainCard from "@components/common/Card/MainCard";
+import styled from "styled-components";
+
+const MainList = (props: any) => {
+  return (
+    <ListContainer>
+      <div className="inner">
+        <CardContainer>
+          <MainCard />
+          <MainCard />
+          <MainCard />
+          <MainCard />
+          <MainCard />
+          <MainCard />
+        </CardContainer>
+      </div>
+    </ListContainer>
+  );
+};
+
+const ListContainer = styled.div`
+  padding: 20px 0;
+`;
+const CardContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  margin-left: -10px;
+  margin-right: -10px;
+`;
+
+export default MainList;
