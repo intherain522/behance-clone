7440f49b04359c3c272298651cae059ac2818208
GY <intherain522@naver.com>
2022-10-27 23:08:23 +0900

    feat : Wrap 추가 구조 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,34 @@
 import MainCard from "@components/common/Card/MainCard";
 import Footer from "@components/common/Footer";
 import Header from "@components/common/Header";
+import MainFilter from "@components/layout/main/MainFilter";
 import MainList from "@components/layout/main/MainList";
 import MainLogo from "@components/layout/main/MainLogo";
 import MainSearch from "@components/layout/main/MainSearch";
 import type { NextPage } from "next";
 import styled from "styled-components";
-
+const Wrap = styled.div``;
 const MainHeader = styled.div`
   width: 100%;
-  position: fixed;
+  position: sticky;
   top: 0;
   left: 0;
   z-index: 500;
+  background-color: #fff;
+  border-bottom: 1px solid #ececec;
 `;
 
 const Index: NextPage = () => {
   return (
-    <MainHeader>
-      <Header />
-      <MainSearch />
+    <Wrap>
+      <MainHeader>
+        <Header />
+        <MainSearch />
+        <MainFilter />
+      </MainHeader>
       <MainList />
       <Footer />
-    </MainHeader>
+    </Wrap>
   );
 };
 
