cb9d2bd459eda13c88a3c96d252efa66c31c1afd
GY <intherain522@naver.com>
2022-10-27 23:07:42 +0900

    design : hover css 수정

--- components/common/Header/Nav.tsx
+++ components/common/Header/Nav.tsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 interface Props {
-  className?: string
+  className?: string;
 }
 
 const Nav = styled(({ className }: Props) => {
@@ -10,7 +10,7 @@ const Nav = styled(({ className }: Props) => {
       <ul>
         <li className="h_logo">
           <a href="/">
-            <img src={'/images/common/main_logo1.png'} alt="로고" />
+            <img src={"/images/common/main_logo1.png"} alt="로고" />
           </a>
         </li>
         <li>
@@ -27,7 +27,7 @@ const Nav = styled(({ className }: Props) => {
         </li>
       </ul>
     </div>
-  )
+  );
 })`
   & ul {
     display: flex;
@@ -39,14 +39,44 @@ const Nav = styled(({ className }: Props) => {
     padding: 19px 15px;
     font-weight: 500;
     font-size: 17px;
+    position: relative;
+    &::before {
+      content: "";
+      width: 100%;
+      height: 2px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background: #222222;
+      opacity: 0;
+    }
+    &:hover::before {
+      animation-name: line;
+      animation-duration: 0.3s;
+      animation-fill-mode: forwards;
+    }
+    @keyframes line {
+      from {
+        opacity: 0;
+        bottom: -2px;
+      }
+      to {
+        opacity: 1;
+        bottom: 0px;
+      }
+    }
   }
+
   & .h_logo {
     width: 82px;
     padding: 0 15px 0 0;
     & img {
       width: 100%;
     }
+    &::before {
+      display: none;
+    }
   }
-`
+`;
 
-export default Nav
+export default Nav;
