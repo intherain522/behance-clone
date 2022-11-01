b0607229c9661eb094aff2f1d2ea0f4ce2a7607f
GY <intherain522@naver.com>
2022-11-01 22:15:05 +0900

    feat : AlignFilter 추가

---
+++ components/common/Filter/AlignFilter.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const AlignFilter = () => {
+  return (
+    <Container>
+      <Text>추천</Text>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: relative;
+  &::before {
+    content: "정렬";
+    position: absolute;
+    top: -14px;
+    left: 5px;
+    font-size: 10px;
+    color: dimgray;
+  }
+`;
+const Text = styled.div`
+  font-size: 16px;
+  padding: 0 5px;
+  &::after {
+    content: "";
+    position: relative;
+    display: inline-block;
+    border-left: 3px solid transparent;
+    border-right: 3px solid transparent;
+    border-top: 3px solid dimgray;
+    height: 0;
+    width: 0;
+    left: 6px;
+    top: -5px;
+  }
+`;
+
+export default AlignFilter;
