28ed09d4cf74d71c8cff9b8acc38714841e37174
GY <intherain522@naver.com>
2022-11-01 22:14:07 +0900

    design : 웹폰트 추가

--- components/common/Config/index.tsx
+++ components/common/Config/index.tsx
@@ -2,11 +2,9 @@ const Config = () => {
   return (
     <>
       <meta charSet="UTF-8" />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0 user-scalable=no"
-      />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0 user-scalable=no" />
       <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/poposnail61/min-sans@main/web/css/minsans-dynamic-subset.css"></link>
     </>
   );
 };
