63f175cd82377f07be66051c7fc25d7a7191f386
GY <intherain522@naver.com>
2022-11-01 22:15:26 +0900

    feat : Common.css 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { AppProps } from "next/app";
 import { QueryClientProvider, QueryClient } from "react-query";
 import GlobalStyle from "../styles/GlobalStyle";
+import Common from "styles/Common";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
+      <Common />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
